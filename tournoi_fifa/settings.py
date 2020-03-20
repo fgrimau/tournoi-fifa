73dbea9f07abae2cbcd407765b3a5c6ed92726d7
fgrimau <florent.grimau@ulb.be>
2020-03-20 22:39:13 +0100

    [Add] statics dir in settings

--- tournoi_fifa/settings.py
+++ tournoi_fifa/settings.py
@@ -99,22 +99,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'fr-be'
+TIME_ZONE = 'Europe/Brussels'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = (
+    "tournoi_fifa/assets/",
+)
