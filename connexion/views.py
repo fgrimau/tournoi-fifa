02358b27362f04eb9ef9b7bf4bd931bf3b68cf22
fgrimau <florent.grimau@outlook.com>
2020-03-21 23:09:38 +0100

    [Add] connection + disconnection + user profile page

--- connexion/views.py
+++ connexion/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
 from connexion.forms import UserForm, CompleteProfileForm
+from django.contrib import messages
+from django.contrib.auth import logout
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 
 def register_view(request, lang=""):
@@ -25,4 +32,45 @@ def complete_profile_view(request, lang=""):
 
 
 def login_view(request, lang=""):
-    return render(request, "login.html")
+    if request.method != "POST":
+        return render(request, 'login.html', locals())
+    else:
+        form = request.POST
+        username = form["username"]
+        password = form["password"]
+
+        print("foferofreofjre :", username)
+
+        try:
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                messages.add_message(
+                    request, messages.SUCCESS,
+                    "Rebonjour {}".format(request.user.first_name))
+                print("Kay")
+                return HttpResponseRedirect("/{}/".format(lang))
+            else:
+                print("noKay")
+                messages.add_message(
+                    request, messages.ERROR,
+                    "Vos identifiants ne correspondent à \
+                    aucun compte !")
+                return HttpResponseRedirect("/{}/".format(lang))
+        except Exception as e:
+            print("Error : ", e)
+
+            messages.add_message(
+                request, messages.ERROR,
+                "Vos identifiants ne correspondent à \
+                aucun compte !")
+        return HttpResponseRedirect("/{}/".format(lang))
+
+
+def disconnect(request, lang=""):
+    logout(request)
+
+    messages.add_message(
+        request, messages.WARNING,
+        "Vous avez été déconnecté")
+    return HttpResponseRedirect("/{}/".format(lang))
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 import connexion.views
+import users.views
 
 urlpatterns = [
     path('', connexion.views.register_view, name="register_view"),
     path('login/', connexion.views.login_view, name="login_view"),
+    path('disconnect/', connexion.views.disconnect, name="disconnect_view"),
+    path('me/', users.views.profile_view, name="profile_view"),
 ]
