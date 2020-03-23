3eef967a96212af13975d73f5a6e9a15d5e3a5ea
fgrimau <florent.grimau@outlook.com>
2020-03-23 16:42:36 +0100

    [Upd] loads of statics

--- users/views.py
+++ users/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="/en/users")
 def profile_view(request, lang=""):
-    return render(request, "profil.html", locals())
+    return render(request, "profil_{}.html".format(lang), locals())
