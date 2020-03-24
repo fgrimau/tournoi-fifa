87bd3846b885cc1acb353cc0c7e48780608e8a1c
yishyu <yishen.yu@ulb.be>
2020-03-24 19:44:32 +0100

    traduction

--- home/views.py
+++ home/views.py
@@ -11,23 +11,23 @@ import secrets
 
 messages_langs = {
     'mail_subj_reinit_fr': "Réinitialisation du mot de passe FIFA-COVID19",
-    'mail_subj_reinit_en': "Réinitialisation du mot de passe FIFA-COVID19",
-    'mail_subj_reinit_nl': "Réinitialisation du mot de passe FIFA-COVID19",
+    'mail_subj_reinit_en': "FIFA-COVID19 password reset",
+    'mail_subj_reinit_nl': "FIFA-COVID19-wachtwoord opnieuw instellen",
     'mail_mess_reinit_fr': "Une demande de réinitialisation de votre mot de passe a été faite pour votre compte sur le site fifa-covid19.be\n \
 Pour continuer la réinitialisation, veuillez suivre ce lien {}\n \
 Si vous n'etes pas à l'origine de cette demande, ou que vous ne possédez pas de compte sur notre site, vous pouvez ignorer ce message",
-    'mail_mess_reinit_en': "Une demande de réinitialisation de votre mot de passe a été faite pour votre compte sur le site fifa-covid19.be\n \
-Pour continuer la réinitialisation, veuillez suivre ce lien {}\n \
-Si vous n'etes pas à l'origine de cette demande, ou que vous ne possédez pas de compte sur notre site, vous pouvez ignorer ce message",
-    'mail_mess_reinit_nl': "Une demande de réinitialisation de votre mot de passe a été faite pour votre compte sur le site fifa-covid19.be\n \
-Pour continuer la réinitialisation, veuillez suivre ce lien {}\n \
-Si vous n'etes pas à l'origine de cette demande, ou que vous ne possédez pas de compte sur notre site, vous pouvez ignorer ce message",
+    'mail_mess_reinit_en': "A request to reset your password was made for your account on the site fifa-covid19.be\n \
+To continue resetting, please follow this link {}\n \
+If you are not the originator of this request, or if you do not have an account on our site, you can ignore this message",
+    'mail_mess_reinit_nl': "Er werd een verzoek ingediend om je wachtwoord opnieuw in te stellen voor je account op de site fifa-covid19.be\n \
+Volg deze link om door te gaan met resetten {}\n \
+Als u niet de initiator van dit verzoek bent, of als u geen account op onze site heeft, kunt u dit bericht negeren",
     'no_user_fr': "Aucun compte n'a été trouvé avec ce pseudonyme !",
     'no_user_en': "No account was found with these credentials !",
-    'no_user_nl': "No account was found with these credentials !",
+    'no_user_nl': "Geen account gevonden met dit pseudoniem !",
     'email_sent_fr': "Un email a été envoyé à votre adresse {} (Vérifiez dans les spams !)",
     'email_sent_en': "An email has been sent to your address {} (Be sure to check your spams !)",
-    'email_sent_nl': "An email has been sent to your address {} (Be sure to check your spams !)",
+    'email_sent_nl': "Er is een e-mail verzonden naar uw adres {} (controleer spam!)",
 }
 
 
