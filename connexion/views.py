b0b9256d0ce6bd8e37467ee4d49add356590a666
yishyu <yishen.yu@ulb.be>
2020-03-24 19:44:32 +0100

    traduction

--- connexion/views.py
+++ connexion/views.py
@@ -10,34 +10,32 @@ from connexion.forms import UserForm, CompleteProfileForm
 messages_langs = {
     'wlcbck_fr': "Rebonjour {} !",
     'wlcbck_en': "Welcome back {} !",
-    'wlcbck_nl': "Welcome back {} !",
+    'wlcbck_nl': "Welkom terug{} !",
     'faillogin_fr': "Vos identifiants ne correspondent à aucun compte !",
     'faillogin_en': "These login does not correspond to any account !",
-    'faillogin_nl': "These login does not correspond to any account !",
+    'faillogin_nl': "Uw identificatiegegevens komen met geen enkel account overeen!",
     'disco_fr': "Vous avez été déconnecté",
     'disco_en': "You've been disconnected",
-    'disco_nl': "You've been disconnected",
+    'disco_nl': "Je bent verbroken",
     'payment_fr': "Afin de finaliser votre inscription, nous vous demandons de\
     faire un don de minimum <strong>6€</strong> au CHU Saint-Pierre via \
     <a href='https://donate.kbs-frb.be/Brussel_UMC_SintPieter/~mon-don?_cv=1'>\
     ce lien</a>, et de \
     nous envoyer un screenshot/une photo par mail à l'adresse \
     <strong>info@fifa-covid19.be</strong>",
-    'payment_en': "Afin de finaliser votre inscription, nous vous demandons de\
-    faire un don de minimum <strong>6€</strong> au CHU Saint-Pierre via \
+    'payment_en': "In order to finalize your registration, we ask you to \
+    make a minimum donation of <strong>6€</strong> at CHU Saint-Pierre via \
     <a href='https://donate.kbs-frb.be/Brussel_UMC_SintPieter/~mon-don?_cv=1'>\
-    ce lien</a>, et de \
-    nous envoyer un screenshot/une photo par mail à l'adresse \
+    this link </a>, and send us a screenshot / photo by email to the address\
     <strong>info@fifa-covid19.be</strong>",
-    'payment_nl': "Afin de finaliser votre inscription, nous vous demandons de\
-    faire un don de minimum <strong>6€</strong> au CHU Saint-Pierre via \
-    <a href='https://donate.kbs-frb.be/Brussel_UMC_SintPieter/~mon-don?_cv=1'>\
-    ce lien</a>, et de \
-    nous envoyer un screenshot/une photo par mail à l'adresse \
+    'payment_nl': "Om uw registratie af te ronden, vragen wij u om \
+    een minimale donatie van <strong>6€</strong> te maken bij CHU Saint-Pierre via \
+    deze link <a href='https://donate.kbs-frb.be/Brussel_UMC_SintPieter/~mon-don?_cv=1'>\
+    </a>, en stuur ons een screenshot / foto per e-mail naar het adres \
     <strong>info@fifa-covid19.be</strong>",
     "understood_fr": "Compris !",
     "understood_en": "Understood !",
-    "understood_nl": "Undertsood !",
+    "understood_nl": "Begrepen !",
 }
 
 
