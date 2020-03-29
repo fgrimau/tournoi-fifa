ba86cc972d6145bb26dd4b79ac0887f931119f28
fgrimau <florent.grimau@outlook.com>
2020-03-30 01:36:24 +0200

    [Add] users profile view

--- users/urls.py
+++ users/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('login/', connexion.views.login_view, name="login_view"),
     path('disconnect/', connexion.views.disconnect, name="disconnect_view"),
     path('me/', users.views.profile_view, name="profile_view"),
+    path('all/', users.views.user_list_view, name="list_user_view"),
+    path(
+        '<str:username>/', users.views.other_profile_view,
+        name="other_profile_view"),
 ]
